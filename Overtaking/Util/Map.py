c737b7699f0602d1c96ff472508cfc9e08ad149b
Ray <raybjork@gmail.com>
2021-03-29 22:00:01 -0400

    reward map implemented

--- Overtaking/Util/Map.py
+++ Overtaking/Util/Map.py
@@ -1,8 +1,12 @@
 import torch
 import yaml
 import numpy as np
+import matplotlib.pyplot as plt
 from PIL import Image
 from argparse import Namespace
+from Overtaking.Util.LocalField import generate_local_affine_grid
+from numba import njit\
+
 
 class Map():
     def __init__(self, config_yaml_path):
@@ -13,13 +17,18 @@ class Map():
         self.path = conf.map_path
         self.ext = conf.map_ext
         self.start_pose = np.array([conf.sx, conf.sy, conf.stheta])
+        self.waypoints = np.loadtxt(conf.wpt_path, 
+                                    delimiter=conf.wpt_delim, 
+                                    skiprows=conf.wpt_rowskip)
+        self.waypoints = np.vstack((self.waypoints[:, conf.wpt_xind], 
+                                    self.waypoints[:, conf.wpt_yind])).T
 
         # open map
         with open(self.path + '.yaml', 'r') as yaml_stream:
             try:
                 map_metadata = yaml.safe_load(yaml_stream)
                 self.resolution = map_metadata['resolution']
-                self.origin = map_metadata['origin']
+                self.origin = np.array(map_metadata['origin'])[:2]
             except yaml.YAMLError as error:
                 print(error)
 
@@ -30,12 +39,34 @@ class Map():
         self.height = self.image.shape[2]
         self.width = self.image.shape[3]
 
+        # calculate reward
+        self.reward = self.image.clone()
+
+        segment_lengths = np.linalg.norm(self.waypoints[1:, :] - self.waypoints[:-1, :], axis=1)
+        for i in range(self.width):
+            for j in range(self.height):
+                if self.reward[0, 0, j, i] > 0:
+                    position = self.resolution * np.array([i, j]) + self.origin
+                    nearest_point, _, _, segment_idx = Map._nearest_point_on_trajectory(position, self.waypoints)
+                    self.reward[0, 0, j, i] = np.sum(segment_lengths[:segment_idx]) + np.linalg.norm(self.waypoints[segment_idx, :] - position)
+
+    def display(self):
+        fig, axes = plt.subplots(nrows=2, figsize=(4, 6))
+        axes.flat[0].set_title("Occupancy Map")
+        im = axes.flat[0].imshow(self.image.squeeze())
+        axes.flat[1].set_title("Reward Map")
+        # im = axes.flat[1].imshow(255 * self.reward.squeeze() / self.reward.max())
+        im = axes.flat[1].imshow(self.reward.squeeze())
+        # waypoints = (self.waypoints - self.origin) / self.resolution
+        # plt.scatter(waypoints[:, 0], waypoints[:, 1])
+        plt.colorbar(im, ax=axes.ravel().tolist())  
+        [ax.set_axis_off() for ax in axes.ravel()]
+        plt.show(block=True)
+
     def sample_obstacles(self, pose, local_grid_size, resolution):
-        sample_grid = Map._generate_local_affine_grid(pose[2], resolution)
+        sample_grid = generate_local_affine_grid(pose[2], resolution)
         map_size = self.resolution * torch.tensor([self.width, self.height])
-        scaled_position = (torch.tensor([pose[0], pose[1]]) - \
-                           torch.tensor([self.origin[0], self.origin[1]])) \
-                          / (map_size / 2)
+        scaled_position = (pose[:2] - self.origin) / (map_size / 2)
         sample_grid = sample_grid * (local_grid_size / map_size) \
                       - torch.tensor([1, 1]) + scaled_position
         local_grid = torch.nn.functional.grid_sample(self.image, 
@@ -46,34 +77,41 @@ class Map():
         return local_grid
 
     @staticmethod
-    def _sample_against_data(data, resolution, local_relative_scale, 
-                            data_scale, pose_delta, null_value):
-        """
-        data: the 2d grid of data that the local field should sample from
-        resolution: number of squares in local grid
-        local_relative_scale: size of the local grid/size of the data grid
-        data_scale: scale of the data in world space
-        pose_delta: difference between poses (x, y, theta)
-        """
-        data_offset = pose_delta[:2] / (data_scale / 2) - torch.tensor([1, 0])
-        aff_g = Map._generate_local_affine_grid(pose_delta[2], resolution) \
-                * local_relative_scale + data_offset
-        sampled_data = torch.nn.functional.grid_sample(data, aff_g.float())
-        off_mask = torch.any(torch.abs(aff_g) > 1, 3).squeeze()
-        sampled_data = sampled_data.squeeze()
-        sampled_data[off_mask] = null_value
-        return sampled_data
+    @njit(fastmath=False, cache=True)
+    def _nearest_point_on_trajectory(point, trajectory):
+        '''
+        Return the nearest point along the given piecewise linear trajectory.
 
-    @staticmethod
-    def _generate_local_affine_grid(theta, resolution):
-        """
-        theta: world angle for the car
-        resolution: number of squares in grid
-        """
-        T = torch.tensor([[torch.cos(theta), -torch.sin(theta), torch.cos(theta)],
-                          [torch.sin(theta), torch.cos(theta), torch.sin(theta)]]).unsqueeze(0)
-        return torch.nn.functional.affine_grid(T, torch.Size((1, 1, resolution + 1, resolution + 1)))
+        Same as nearest_point_on_line_segment, but vectorized. This method is quite fast, time constraints should
+        not be an issue so long as trajectories are not insanely long.
+
+            Order of magnitude: trajectory length: 1000 --> 0.0002 second computation (5000fps)
+
+        point: size 2 numpy array
+        trajectory: Nx2 matrix of (x,y) trajectory waypoints
+            - these must be unique. If they are not unique, a divide by 0 error will destroy the world
+        '''
+        diffs = trajectory[1:, :] - trajectory[:-1, :]
+        l2s   = diffs[:, 0] ** 2 + diffs[:, 1] ** 2
+        # this is equivalent to the elementwise dot product
+        # dots = np.sum((point - trajectory[:-1,:]) * diffs[:,:], axis=1)
+        dots = np.empty((trajectory.shape[0] - 1,))
+        for i in range(dots.shape[0]):
+            dots[i] = np.dot((point - trajectory[i, :]), diffs[i, :])
+        t = dots / l2s
+        t[t < 0.0] = 0.0
+        t[t > 1.0] = 1.0
+        # t = np.clip(dots / l2s, 0.0, 1.0)
+        projections = trajectory[:-1,:] + (t*diffs.T).T
+        # dists = np.linalg.norm(point - projections, axis=1)
+        dists = np.empty((projections.shape[0],))
+        for i in range(dists.shape[0]):
+            temp = point - projections[i]
+            dists[i] = np.sqrt(np.sum(temp*temp))
+        min_dist_segment = np.argmin(dists)
+        return projections[min_dist_segment], dists[min_dist_segment], t[min_dist_segment], min_dist_segment
 
 
 if __name__ == '__main__':
-    Map('config_example_map_filled.yaml')
+    Map('config_example_map_filled.yaml').display()
+    
